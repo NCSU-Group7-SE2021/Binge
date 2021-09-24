ac5ee339a90156ccc70304f80166623dd011fb02
Vignesh Muthukumar <43530800+vickymhs@users.noreply.github.com>
2021-09-24 18:36:41 -0400

    Cast screen css (#8)
    * Adding cast details in the watch screen
    
    * Added css and fetching movie title in cast screen
    
    * Added cast fetching based on title - incomplete
    
    * Rebased and resolved merge conflicts with main
    
    Co-authored-by: Rahul Kalita <rahulkalita8@gmail.com>

--- server/axios_api.js
+++ server/axios_api.js
@@ -1,28 +1,26 @@
 const { default: axios } = require("axios");
-var API_KEY="631772a5";
+var API_KEY = "631772a5";
 
 
-const getRatings = async(title) => {
-    var URL=`https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
+const getRatings = async (title) => {
+    var URL = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${title}`;
     const ratings = {
         RottenTomatoes: "",
         IMDB: ""
     };
     try {
-         const response = await axios.get(URL)
-         const data = response.data.Ratings;
-         data.forEach(rating => {
-             if (rating.Source=="Internet Movie Database"){
-                ratings.IMDB=rating.Value;
-             }
-             if (rating.Source=="Rotten Tomatoes"){
-                 ratings.RottenTomatoes = rating.Value;
+        const response = await axios.get(URL)
+        const data = response.data.Ratings;
+        data.forEach(rating => {
+            if (rating.Source == "Internet Movie Database") {
+                ratings.IMDB = rating.Value;
             }
-         });
-         } catch(errors){
-             console.error(errors);
-         }
-    return ratings;
+            if (rating.Source == "Rotten Tomatoes") {
+                ratings.RottenTomatoes = rating.Value;
+            }
+        });
+    } catch (errors) {
+        console.error(errors);
     }
-
-
+    return ratings;
+}
