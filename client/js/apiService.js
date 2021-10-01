10a8713b26f2bdd2d1e1c310365615c1f2494d11
Eshita Arza <eshita.arza@gmail.com>
2021-09-30 23:30:19 -0400

    Added demo images

--- client/js/apiService.js
+++ client/js/apiService.js
@@ -7,11 +7,11 @@ const SERVER_BUSY = "Server busy";
  * @returns Rotten Tomatoes rating
  */
 const extractRottenTomatoRating = (ratingsArray) => {
-  if (ratingsArray.length > 0) {
-    const rottenTomatoRating = ratingsArray[1] ? ratingsArray[1].Value : "N/A";
-    return rottenTomatoRating;
-  }
-  return "N/A";
+    if (ratingsArray.length > 0) {
+        const rottenTomatoRating = ratingsArray[1] ? ratingsArray[1].Value : "N/A";
+        return rottenTomatoRating;
+    }
+    return "N/A";
 };
 
 /**
@@ -20,35 +20,33 @@ const extractRottenTomatoRating = (ratingsArray) => {
  * @returns detailed information about the title - ratings, reviews, cast
  */
 const fetchRatings = async (title) => {
-  // we will add functionality for user to give their keys using popup.html
-  // we can discuss scope. we can do it or it can be added as future scope.
-  let omdbApiKey = (await getApiKeyFromStorage()) || "631772a5";
-  let defaultRatings = {
-    imdbRating: "N/A",
-    rottenTomato: "N/A",
-    cast: "N/A",
-    reviews: []
-  };
+    let omdbApiKey = (await getApiKeyFromStorage()) || "631772a5";
+    let defaultRatings = {
+        imdbRating: "N/A",
+        rottenTomato: "N/A",
+        cast: "N/A",
+        reviews: []
+    };
 
-  try {
-    const omdbResponse = await fetch(encodeURI(`https://www.omdbapi.com/?apikey=${omdbApiKey}&t=${title}`));
+    try {
+        const omdbResponse = await fetch(encodeURI(`https://www.omdbapi.com/?apikey=${omdbApiKey}&t=${title}`));
 
-    const omdbResponseJson = await omdbResponse.json();
-    if (omdbResponseJson["Error"] && omdbResponseJson["Error"] === MOVIE_NOT_FOUND) {
-      console.log("OMDB unable to find given movie");
-    } else {
-      defaultRatings = {
-        ...defaultRatings,
-        imdbRating: omdbResponseJson.imdbRating,
-        rottenTomato: extractRottenTomatoRating(omdbResponseJson.Ratings || []),
-        cast: omdbResponseJson.Actors,
-        reviews: await fetchReviews(omdbResponseJson.imdbID)
-      };
+        const omdbResponseJson = await omdbResponse.json();
+        if (omdbResponseJson["Error"] && omdbResponseJson["Error"] === MOVIE_NOT_FOUND) {
+            console.log("OMDB unable to find given movie");
+        } else {
+            defaultRatings = {
+                ...defaultRatings,
+                imdbRating: omdbResponseJson.imdbRating,
+                rottenTomato: extractRottenTomatoRating(omdbResponseJson.Ratings || []),
+                cast: omdbResponseJson.Actors,
+                reviews: await fetchReviews(omdbResponseJson.imdbID)
+            };
+        }
+    } catch (error) {
+        console.log(`Failed to fetch rating: ${error}`);
     }
-  } catch (error) {
-    console.log(`Failed to fetch rating: ${error}`);
-  }
-  return defaultRatings;
+    return defaultRatings;
 };
 
 /**
@@ -57,17 +55,17 @@ const fetchRatings = async (title) => {
  * @returns Object with metacritic reviews and publisher
  */
 const extractReview = async (reviewsArray) => {
-  let data = [];
-  if (reviewsArray["items"] && reviewsArray["items"].length != 0) {
-    for (item in reviewsArray["items"]) {
-      let object = {
-        publisher: reviewsArray["items"][item].publisher,
-        review: reviewsArray["items"][item].content
-      };
-      data.push(object);
+    let data = [];
+    if (reviewsArray["items"] && reviewsArray["items"].length != 0) {
+        for (item in reviewsArray["items"]) {
+            let object = {
+                publisher: reviewsArray["items"][item].publisher,
+                review: reviewsArray["items"][item].content
+            };
+            data.push(object);
+        }
     }
-  }
-  return data;
+    return data;
 };
 
 /**
@@ -76,41 +74,39 @@ const extractReview = async (reviewsArray) => {
  * @returns Object with metacritic reviews and publisher
  */
 const fetchReviews = async (imdbId) => {
-  // we will add functionality for user to give their keys using popup.html
-  // we can discuss scope. we can do it or it can be added as future scope.
-  let reviews = [
-    {
-      publisher: "",
-      review: ""
+    let reviews = [
+        {
+            publisher: "",
+            review: ""
+        }
+    ];
+
+    let imdbApiKey = (await getImdbApiKeyFromStorage()) || "k_0afyx7ti";
+
+    try {
+        const imdbResponse = await fetch(
+            encodeURI(`https://imdb-api.com/en/API/MetacriticReviews/${imdbApiKey}/${imdbId}`)
+        );
+
+        const imdbResponseJson = await imdbResponse.json();
+
+        if (
+            imdbResponseJson["errorMessage"] &&
+            (imdbResponseJson["errorMessage"] === SERVER_BUSY || imdbResponseJson["errorMessage"] != null)
+        ) {
+            console.log("IMDB unable to find metacritic reviews for the given movie " + imdbId);
+        } else {
+            reviews = extractReview(imdbResponseJson);
+        }
+    } catch (error) {
+        console.log(`Failed to fetch reviews: ${error}`);
     }
-  ];
-
-  let imdbApiKey = (await getImdbApiKeyFromStorage()) || "k_0afyx7ti";
-
-  try {
-    const imdbResponse = await fetch(
-      encodeURI(`https://imdb-api.com/en/API/MetacriticReviews/${imdbApiKey}/${imdbId}`)
-    );
-
-    const imdbResponseJson = await imdbResponse.json();
-
-    if (
-      imdbResponseJson["errorMessage"] &&
-      (imdbResponseJson["errorMessage"] === SERVER_BUSY || imdbResponseJson["errorMessage"] != null)
-    ) {
-      console.log("IMDB unable to find metacritic reviews for the given movie " + imdbId);
-    } else {
-      reviews = extractReview(imdbResponseJson);
-    }
-  } catch (error) {
-    console.log(`Failed to fetch reviews: ${error}`);
-  }
-  return reviews;
+    return reviews;
 };
 
 module.exports = {
-  extractRottenTomatoRating,
-  fetchRatings,
-  extractReview,
-  fetchReviews
+    extractRottenTomatoRating,
+    fetchRatings,
+    extractReview,
+    fetchReviews
 };
